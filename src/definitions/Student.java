2936f48e8d82c6a616e176148ee4a92517dde12c
gautam <gautam.sethi_cs19@gla.ac.in>
2020-08-28 01:19:00 +0530

    create getters and setters for fields in Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -7,4 +7,61 @@
 package definitions;
 
 public class Student {
+    String firstName;
+    String middleName;
+    String lastName;
+    long universityRollNo;
+    int noOfBooksIssued;
+    private Book[] book;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getUniversityRollNo() {
+        return universityRollNo;
+    }
+
+    public void setUniversityRollNo(long universityRollNo) {
+        this.universityRollNo = universityRollNo;
+    }
+
+    public int getNoOfBooksIssued() {
+        return noOfBooksIssued;
+    }
+
+    public void setNoOfBooksIssued(int noOfBooksIssued) {
+        this.noOfBooksIssued = noOfBooksIssued;
+    }
+
+    public Book[] getBook() {
+        return book.clone();
+    }
+
+    public void setBook(Book[] book) {
+        this.book = book;
+    }
+
+
 }
